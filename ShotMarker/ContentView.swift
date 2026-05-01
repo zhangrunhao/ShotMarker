74b9b978d339ab7b019db61af20185e683b3bd20
zhangrh <runhaozhang@sohu-inc.com>
2026-05-01 13:07:12 +0800

    Initial Commit

---
+++ ShotMarker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ShotMarker
+//
+//  Created by runhao zhang on 2026/5/1.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ShotMarker/ShotMarkerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShotMarkerApp.swift
+//  ShotMarker
+//
+//  Created by runhao zhang on 2026/5/1.
+//
+
+import SwiftUI
+
+@main
+struct ShotMarkerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
